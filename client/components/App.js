7c1b1b060363e6f54e1171b5527c43d82c8a127d
mikkelfridn <miknoergaard@deloitte.dk>
2018-08-24 15:10:45 +0200

    add support for live development

---
+++ client/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from './welcome';
+
+class App extends React.Component {
+    render(){
+        return (
+            <Welcome />
+        );
+    }
+}
+
+export default App;
